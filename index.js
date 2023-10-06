31f1203d96529a5aa361a5e57d5be8bf294a860a
saikiran-C6 <saikiranp49@gmail.com>
2023-10-06 15:33:45 +0530

    commit all changes

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
 
 // use express router
 app.use('/', require('./routes'));
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function(err){
     if(err){
