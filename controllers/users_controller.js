a0fd969c69f913d1c6df5de1de4e8bef8963e401
saikiran-C6 <saikiranp49@gmail.com>
2023-11-02 23:09:37 +0530

    commit all

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -35,24 +35,54 @@ module.exports.profile = async function(req, res) {
 };
 
 module.exports.update = async function (req, res) {
-  try {
-      const user = await User.findById(req.params.id).exec();
+//   try {
+//       const user = await User.findById(req.params.id).exec();
 
-      if (!user) {
-          return res.status(404).send('User not found');
-      }
+//       if (!user) {
+//           return res.status(404).send('User not found');
+//       }
 
-      if (user.id === req.user.id) {
-          // Update the user
-          await User.findByIdAndUpdate(req.params.id, req.body).exec();
-          return res.redirect('back');
-      } else {
-          return res.status(401).send('Unauthorized');
-      }
-  } catch (err) {
-      console.error("Error:", err);      
+//       if (user.id === req.user.id) {
+//           // Update the user
+//           await User.findByIdAndUpdate(req.params.id, req.body).exec();
+//           return res.redirect('back');
+//       } else {
+//           return res.status(401).send('Unauthorized');
+//       }
+//   } catch (err) {
+//       console.error("Error:", err);      
+//   }
+// };
+
+  if(req.user.id == req.params.id){
+
+    try{
+
+      let user = await User.findById(req.params.id);
+      User.uploadedAvatar(req, res, function(err){
+        if(err) {console.log('******Multer Error: ', err)}
+
+        user.name = req.body.name;
+        user.email = req.body.email;
+
+        if(req.file){
+          // this is saving the path of the uploaded file into the avatar filed in the user
+          user.avatar = User.avatarPath + '/' + req.file.filename;
+        }
+        user.save();
+        return res.redirect('back');
+      });
+    }
+    catch(err){
+      req.flash('error', err);
+      return res.redirect('back');
+    }
+
+  }else{
+    req.flash('error', 'Unauthorized');
+    return res.status(401).send('Unauthorized');
   }
-};
+}
 
 
 // render the sign up page
